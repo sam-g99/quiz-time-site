b4ead38d579fc6b3f608ccf470067743427540bb
Sam G <46730010+sam-g99@users.noreply.github.com>
2019-12-29 05:48:03 -0500

    single player no css

--- frontend/src/pages/TakeQuiz.vue
+++ frontend/src/pages/TakeQuiz.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <p>{{ desc }}</p>
+    <h1>{{ quiz.title }}</h1>
+    <p>{{ quiz.desc }}</p>
+    <p>Amount:{{ quiz.amount }}</p>
     <p>{{ timeLeft }}</p>
     <div class="question">
-      <h3>Question 1</h3>
-      <h4>{{ question.question.text }}</h4>
-      <div v-for="option in question.options" :key="option.id">
+      <h3>Question {{ question.number }}</h3>
+      <h4>{{ question.text }}</h4>
+      <div
+        v-for="option in question.options"
+        :key="option.id"
+        :class="{ right: right, wrong: wrong }"
+      >
         <input
           :id="option.id"
           name="test"
@@ -27,32 +32,50 @@ import io from 'socket.io-client';
 export default {
   data: () => {
     return {
+      quiz: {},
       title: '',
       desc: '',
       question: {},
+      mode: 'single',
+      amount: 0,
+      right: null,
+      wrong: null,
       socket: io('http://192.168.1.7:3001'),
       timeLeft: 0,
       answerPending: false,
     };
   },
   mounted() {
-    this.socket.emit('quizTime', this.$route.params.id);
+    this.socket.emit('quizTime', {
+      id: this.$route.params.id,
+      mode: this.mode,
+    });
 
-    this.socket.on('quiz', quiz => {
-      const { title, desc, question } = quiz;
-      console.log(quiz);
-      this.title = title;
-      this.desc = desc;
-      this.question = question;
+    this.socket.on('quiz', data => {
+      console.log(data);
+      this.quiz = data.info;
+      this.question = data.question;
     });
 
     this.socket.on('question', question => {
-      const { data } = question;
-      this.question = data;
+      this.question = question;
+      this.right = null;
+      this.wrong = null;
+      this.answerPending = false;
     });
 
     this.socket.on('timer', timer => {
       this.timeLeft = timer;
+    });
+    this.socket.on('done', () => {
+      alert('You are done!');
+    });
+    this.socket.on('result', isCorrect => {
+      if (isCorrect) {
+        this.right = true;
+      } else {
+        this.wrong = true;
+      }
     });
   },
   methods: {
@@ -64,4 +87,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.right {
+  background: green;
+}
+
+.wrong {
+  background: red;
+}
+</style>
